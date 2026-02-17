5009c1cad45a7ee912793f56b05040e8f9d64547
Алексей Шашин <alexshashin.2023@gmail.com>
2026-02-17 21:46:55 +0500

    Разработанный контекст для загрузки данных. Шашин

---
+++ Context/TasksContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using API_Shashin1.Model;
+using System;
+
+namespace API_Shashin1.Context
+{
+    public class TasksContext : DbContext
+    {
+        public DbSet<Task> Tasks {  get; set; }
+        public TasksContext()
+        {
+            Database.EnsureCreated();
+            Tasks.Load();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("server=127.0.0.1;uid=root;pwd=;database=TaskManager", new MySqlServerVersion(new Version(8, 0, 11)));
+        }
+    }
+}
