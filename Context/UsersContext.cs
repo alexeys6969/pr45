e67b9aa7c4bcaa4bcc6bdb04c0632ffbf6adf7da
Алексей Шашин <alexshashin.2023@gmail.com>
2026-02-27 18:05:13 +0500

    Создание модели и контекста пользователя. Шашин

---
+++ Context/UsersContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using API_Shashin11.Model;
+using System;
+
+namespace API_Shashin11.Context
+{
+    public class UsersContext : DbContext
+    {
+        public DbSet<Users> Users {  get; set; }
+        public UsersContext()
+        {
+            Database.EnsureCreated();
+            Users.Load();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("server=127.0.0.1;uid=root;pwd=;database=TaskManager", new MySqlServerVersion(new Version(8, 0, 11)));
+        }
+    }
+}
---
+++ Model/Users.cs
@@ -0,0 +1,13 @@
+﻿namespace API_Shashin11.Model
+{
+    public class Users
+    {
+        ///<summary>Код пользователя</summary>
+        public int Id { get; set; }
+        ///<summary>Логин пользователя</summary>
+        public string Login { get; set; }
+        ///<summary>Пароль пользователя</summary>
+        public string Password { get; set; }
+
+    }
+}
